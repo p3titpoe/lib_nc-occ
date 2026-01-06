a4a59709b2a47fb17d3b937dd36b503191920906
petrit <petrit@rimshot.lu>
2026-01-06 11:02:49 +0100

    --- Change struxture

--- start.py
+++ start.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
-from .nc_occ import common
+from lib_occ.nc_occ import common
 from pprint import pp
 
-tt = tag.NcOccTag()
+tt = common.NcOccCommon()
 rs = tt.lists()
 
 print(rs)
